610626a166fa0efbf0152646f5cb74e5a488db22
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-20 22:51:52 +0700

    js lab 64

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// write code here
+let user = {
+  name: 'John',
+  sayHi: function () {
+    console.log(this.name);
+  }
+};
+
+(user.sayHi)(); // * John
+// this is a object before the dott == user
